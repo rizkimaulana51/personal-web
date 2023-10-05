fcf14c5826933a3d2a035d711b7c0c04664868bb
rizkimaulana51 <rizkimaw51@gmail.com>
2023-10-05 15:31:03 +0700

    add gsap animation

--- main/script.js
+++ main/script.js
@@ -1,17 +1,14 @@
 let funcHamburger = () => {
-    
-    const tombolNav = document.querySelector(".hamburger");
-    const menu = document.querySelector(".bar");
-    const menuMobile = document.querySelector(".nav-mobile");
-    tombolNav.addEventListener('click', function() {
-    
-        tombolNav.classList.toggle("active");
-        menu.classList.toggle("active");
-        menuMobile.classList.toggle("active");
-    });
-
-}
+	const tombolNav = document.querySelector('.hamburger');
+	const menu = document.querySelector('.bar');
+	const menuMobile = document.querySelector('.nav-mobile');
+	tombolNav.addEventListener('click', function () {
+		tombolNav.classList.toggle('active');
+		menu.classList.toggle('active');
+		menuMobile.classList.toggle('active');
+	});
+};
 
 funcHamburger();
 
-
+gsap.from('.description', { duration: 3, opacity: 0, ease: 'rough' });
